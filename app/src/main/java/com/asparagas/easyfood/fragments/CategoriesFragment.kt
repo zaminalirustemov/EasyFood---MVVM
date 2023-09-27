7e0a2719d4d363df6792c7d96f2abfd330af42ff
Zaminali <2g17rck@code.eddu.az>
2023-09-27 09:33:12 +0400

    Added Category Fragment

--- app/src/main/java/com/asparagas/easyfood/fragments/CategoriesFragment.kt
+++ app/src/main/java/com/asparagas/easyfood/fragments/CategoriesFragment.kt
@@ -5,19 +5,54 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.asparagas.easyfood.R
+import com.asparagas.easyfood.activities.MainActivity
+import com.asparagas.easyfood.adapters.CategoriesAdapter
+import com.asparagas.easyfood.databinding.FragmentCategoriesBinding
+import com.asparagas.easyfood.pojo.Category
+import com.asparagas.easyfood.viewModel.HomeViewModel
 
 class CategoriesFragment : Fragment() {
 
+    private lateinit var binding: FragmentCategoriesBinding
+    private lateinit var categoriesAdapter: CategoriesAdapter
+    private lateinit var viewModel: HomeViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
+
+        categoriesAdapter = CategoriesAdapter()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categories, container, false)
+    ): View {
+        binding = FragmentCategoriesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        prepareRecyclerView()
+
+        observeCategories()
+    }
+
+    private fun observeCategories() {
+        viewModel.observeCategoriesLiveData().observe(viewLifecycleOwner){categories->
+            categoriesAdapter.setCategories(categories as ArrayList<Category>)
+        }
+    }
+
+    private fun prepareRecyclerView() {
+        binding.rcCategories.apply {
+            layoutManager = GridLayoutManager(context, 3, GridLayoutManager.VERTICAL, false)
+            adapter = categoriesAdapter
+        }
     }
 }
